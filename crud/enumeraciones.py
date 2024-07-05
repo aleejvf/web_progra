96fadf96bab1a0600383ce00dda39f7297117eb1
felGutierrez <fi.gutierrez@duocuc.cl>
2024-07-05 06:51:17 -0400

    carro listo

---
+++ crud/enumeraciones.py
@@ -0,0 +1,19 @@
+
+# LISTA DE RANGOS DE PRECIOS
+PRICE_RANGES = [
+    ('0-50', '0 - 50'),
+    ('51-100', '51 - 100'),
+    ('101-200', '101 - 200'),
+    ('201', '201+'),
+]
+TIPO_CHOICES = [
+        ('ANILLO', 'Anillo'),
+        ('ARO', 'Aro'),
+        ('COLLAR', 'Collar'),
+]
+    
+TAMANO_CHOICES = [
+        ('GRANDE', 'Grande'),
+        ('MEDIANO', 'Mediano'),
+        ('PEQUEÑO', 'Pequeño'),
+]
